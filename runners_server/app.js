7543ff7a4d22176b0278e222f3c525151383bab6
ORANZINO <hupajupa61@gmail.com>
2020-07-09 04:15:16 +0900

    [태진] app.js 소켓 추가

--- runners_server/app.js
+++ runners_server/app.js
@@ -17,6 +17,8 @@ server.on('listening', onListening);
 
 app.set('port', port);
 
+app.io = require('socket.io')(server);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
