5fb6c712ca096b0cf95a92add67b3d1f996abb5f
ORANZINO <hupajupa61@gmail.com>
2020-07-11 18:32:48 +0900

    [태진] log 추가

--- runners_server/models/matchingModel.js
+++ runners_server/models/matchingModel.js
@@ -30,6 +30,9 @@ const match = {
             const lose_query = `SELECT COUNT(if((user_idx="${idx}" AND (result=1 OR result=2)), 1, null)) as lose FROM run`;
             const win_result = await queryParam(win_query);
             const lose_result = await queryParam(lose_query);
+            console.log("User Result: ", user_result);
+            console.log("Win Result: ", win_result);
+            console.log("Lose Result: ", lose_result);
             const final_result = {
                 name: user_result[0].nickname,
                 level: user_result[0].level,
