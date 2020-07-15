d5a49ae2af31421e29c4f3644fc8ea5621fe3fa6
ORANZINO <hupajupa61@gmail.com>
2020-07-15 22:48:19 +0900

    [태진] timeOver 파라미터 제거

--- runners_server/modules/socket.js
+++ runners_server/modules/socket.js
@@ -111,18 +111,9 @@ module.exports = matching => {
                             matching.to(socket.id).emit("timeLeft", socket.adapter.rooms[roomName].leftTime);
                         }
                         else if (socket.adapter.rooms[roomName].leftTime <= 0) {
-                            const user = socket.adapter.rooms[roomName].userList.find(user => user.id === socket.id);
-                            const time = socket.adapter.rooms[roomName].time;
-                            const wantGender = user.wantGender;
-                            const level = user.level;
                             clearInterval(intervalId);
                             socket.leave(roomName, () => {
-                                if (user.win === 0 && user.lose === 0) {
-                                    matching.to(socket.id).emit("timeOver", 0, time, wantGender, level);
-                                }
-                                else {
-                                    matching.to(socket.id).emit("timeOver", 1, time, wantGender, level);
-                                }
+                                matching.to(socket.id).emit("timeOver");
                             })
                         }
                     }, 3000);
