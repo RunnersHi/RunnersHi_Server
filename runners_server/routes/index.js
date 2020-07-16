0c21c369b46bec490913440b36d35bb9d13b76c5
ORANZINO <hupajupa61@gmail.com>
2020-07-16 14:37:17 +0900

    [태진] 로그 추가 buffer 삭제

--- runners_server/routes/index.js
+++ runners_server/routes/index.js
@@ -1,12 +1,9 @@
 const fs = require("fs");
-const iconv = require('iconv-lite');
 const list = fs
     .readdirSync(__dirname)
     .filter(dir => !dir.match(/(^\.)|index/i));
 
 module.exports = (app) => {
-  let code = iconv.encode(new Buffer("밍"), "EUC-KR");
-  let result = iconv.decode(Buffer.from(code), "EUC-KR");
   console.log(code);
   console.log(result);
   for (let ctrl of list) {
